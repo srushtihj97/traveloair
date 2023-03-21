01139703f1225f7cfb07349eb24fd0ccba8c4fc9
srushti-kankotiya <105891223+srushti-kankotiya@users.noreply.github.com>
2023-03-21 15:33:04 -0400

    Update models.py

--- traveloair/api/models.py
+++ traveloair/api/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from datetime import date, time
 
-
-# Create your models here.
 class Flight(models.Model):
     number = models.CharField(max_length=100,
                               unique=True,
